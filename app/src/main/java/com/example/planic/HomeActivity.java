d4ac7231ce5f53429b430b84cff2550c3b977662
Muhammad Ronan Aflah <ronanaflah22.7i@gmail.com>
2025-06-15 03:27:16 +0700

    Tambahan dari Saya (Ronan)

--- app/src/main/java/com/example/planic/HomeActivity.java
+++ app/src/main/java/com/example/planic/HomeActivity.java
@@ -2,7 +2,6 @@ package com.example.planic;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -28,22 +27,6 @@ public class HomeActivity extends AppCompatActivity {
     private TaskAdapter taskAdapter;
     private List<TaskModel> taskList;
     private DatabaseReference taskRef;
-
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-            );
-        }
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
